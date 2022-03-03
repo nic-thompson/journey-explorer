783f904ac0ff23b18ee2ec3605223ddadc8ab459
Nicolas Thompson <mail@nicolasthompson.com>
2022-03-03 11:28:38 +0000

    Adds Journey component to root App component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Journey from "./components/Journey";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Journey Explorer</h1>
+      <Journey />
     </div>
   );
-}
+};
 
 export default App;
