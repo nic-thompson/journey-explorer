b91a800ff68242cc22a51d2cc3ec585fe700fd1a
Nicolas Thompson <mail@nicolasthompson.com>
2022-03-03 11:15:15 +0000

    Map component

---
+++ src/components/Map.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { MapContainer, TileLayer } from "react-leaflet";
+import RouteMachine from "./RouteMachine";
+import { useState, useEffect } from "react";
+
+const Map = ({ stage }) => {
+  const [currentStage, setCurrentStage] = useState([]);
+
+  useEffect(() => {
+    setCurrentStage(stage);
+  }, [stage]);
+
+
+  return (
+    <MapContainer
+      doubleClickZoom={false}
+      id="mapId"
+      zoom={13}
+      center={[51.505, -0.09]}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <RouteMachine stage={currentStage} key={JSON.stringify(currentStage)}/>
+    </MapContainer>
+  );
+};
+
+export default Map;
