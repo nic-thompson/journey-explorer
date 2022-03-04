c03591a4313ef8d0a9bff95d4e7f947034e274b1
Nicolas Thompson <mail@nicolasthompson.com>
2022-03-04 16:53:15 +0000

    Imports MapOne into App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
-import Journey from "./components/Journey";
+import MapOne from "./components/MapOne";
+// import MapThree from "./components/MapThree";
 
 const App = () => {
   return (
     <div>
       <h1>Journey Explorer</h1>
-      <Journey />
+      <MapOne />
+      {/* <MapThree /> */}
     </div>
   );
 };
