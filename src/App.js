b8c40ecaf0564be61095a2661cf7832727a72c31
Nicolas Thompson <mail@nicolasthompson.com>
2022-03-07 12:12:13 +0000

    Wraps Routes in Layout and Switch

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
-// import SingleJourney from "./components/SingleJourney";
+import Welcome from "./components/Welcome";
+import SingleJourney from "./components/SingleJourney";
+import MultipleJourneys from "./components/MultipleJourneys";
 import JourneyExplorer from "./components/JourneyExplorer";
+import Layout from "./components/layout/Layout";
 
 const App = () => {
   return (
-    <div>
-      <h1>Journey Explorer</h1>
-      {/* <SingleJourney /> */}
-      <JourneyExplorer />
-    </div>
+    <Layout>
+      <Switch>
+        <Route exact path="/" component={Welcome} />
+        <Route exact path="/single-journey" component={SingleJourney} />
+        <Route exact path="/multiple-journeys" component={MultipleJourneys} />
+        <Route exact path="/journey-explorer" component={JourneyExplorer} />
+      </Switch>
+    </Layout>
   );
 };
 
