885d3df63e88ffd726152970284388a623bc1461
Nicolas Thompson <mail@nicolasthompson.com>
2022-03-07 12:11:25 +0000

    Adds MultipleJourneys componet

---
+++ src/components/MultipleJourneys.js
@@ -0,0 +1,12 @@
+import css from "./css/MultipleJourneys.module.css";
+
+const MultipleJourneys = () => {
+  return (
+    <div className={css['multiple-journeys']}>
+      <h2>Multiple Journeys</h2>
+      <p>This is an app</p>
+    </div>
+  );
+}
+
+export default MultipleJourneys;
