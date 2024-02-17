a7919854dd4b87ff706c2d7deb7ebeb3df80bfc4
Nicolas Thompson <mail@nicolasthompson.com>
2024-02-17 15:07:52 +0000

    Adds to readme

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ A UI for mapping journey coordinates.
 Install: npm i
 Start: npm start
 
-
+![img](application_screenshot.png)
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,12 +1,11 @@
-import css from "./css/Welcome.module.css";
+import css from './css/Welcome.module.css';
 
 const Welcome = () => {
   return (
     <div className={css.welcome}>
-      <h2>Welcome</h2>
-      <p>This is an app</p>
+      <h2>Welcome to Journey explorer.</h2>
     </div>
   );
-}
+};
 
 export default Welcome;
